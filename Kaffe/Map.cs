8e97e693a7fc10f9b146a0fd522ac581aa7bfcf1
Johan Svensson <johan.svensson93@hotmail.se>
2017-10-06 23:00:27 +0200

    Added Map Class file and created 2 for-loops

---
+++ Kaffe/Map.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffe
+{
+    class Map
+    {
+        public static void mapArray(int[,] array)
+        {
+            for (int x = 0; x < array.GetLength(0); x++ )
+            {
+                Console.WriteLine("#");
+                for (int y = 0; y < array.GetLength(1); y++ )
+                {
+                    Console.WriteLine("#");
+                }
+            }
+        }
+    }
+}
