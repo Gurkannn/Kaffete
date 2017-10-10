98524c6b03861ab3e280541d48ad9c8e7302063a
Johan Svensson <johan.svensson93@hotmail.se>
2017-10-10 13:22:52 +0200

    Removed unnecessary comments and class files.

--- Kaffe/Program.cs
+++ Kaffe/Program.cs
@@ -99,8 +99,6 @@ namespace Kaffe
 
         static void Main(string[] args)
         {
-            Level.LevelStates levelStates1 = Level.LevelStates.level1;
-            Level.GameStates curGameStates = Level.GameStates.intro;
             Console.CursorVisible = false;
             GameIntro();
 
@@ -111,48 +109,7 @@ namespace Kaffe
                 GameLoop();
             }
         }
-        //switch (curGameStates)
-        //{
-        //    case Level.GameStates.intro:
-        //        Console.SetCursorPosition(Console.WindowWidth / 7, Console.WindowHeight / 2);
-        //        Console.ForegroundColor = ConsoleColor.DarkMagenta;
-        //        Console.WriteLine("Try and get as many objects as possible for higher score by using 'W' 'A' 'S' 'D'");
-        //        Console.ReadKey();
-        //        curGameStates = Level.GameStates.ingame;
-        //        break;
-        //    case Level.GameStates.ingame:
-        //        switch (levelStates1)
-        //        {
-        //            case Level.LevelStates.level1:
-        //                Console.SetCursorPosition(0, 0);
-
-        //                switch (Console.ReadKey().KeyChar.ToString())
-        //                {
-
-        //                    case "w":
-        //                        if (Map.Player.CurrentDirection != Direction.South)
-        //                            Map.InputDirection = Direction.North;
-        //                        break;
-        //                    case "a":
-        //                        if (Map.Player.CurrentDirection != Direction.East)
-        //                            Map.InputDirection = Direction.West;
-        //                        break;
-        //                    case "s":
-        //                        if (Map.Player.CurrentDirection != Direction.North)
-        //                            Map.InputDirection = Direction.South;
-        //                        break;
-        //                    case "d":
-        //                        if (Map.Player.CurrentDirection != Direction.West)
-        //                            Map.InputDirection = Direction.East;
-        //                        break;
-        //                    default:
-        //                        break;
-        //                }
-        //                break;
-        //        }
-        //        break;
-
-        //}
+       
     }
 }
 
